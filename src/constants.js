d04ffc01645e4a243cdb5afd0b4d5a8ec7b77eaa
SantiagoMunarOrtiz <160024794+SantiagoMunarOrtiz@users.noreply.github.com>
2024-07-08 17:52:45 -0500

    Primer commit

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `Esta es mi PC. Trabajo principalmente en JavaScript/TypeScript en estos días.
+    He hecho un par de proyectos en estos lenguajes. También me gusta java. De todos modos, independientemente del lenguaje, simplemente me gusta programar.
+    Aquí está mi <a href="https://github.com/SantiagoMunarOrtiz" target="_blank">GitHub</a>!`,
+  "cs-degree": `Este es mi título en Ingeniería de Sistemas. Lo colgué en la pared porque estoy orgulloso de él. Fue un título muy teórico, pero creo que me dio una buena base.`,
+  "sofa-table": `Ese es mi sofá. Me gusta relajarme aquí y ver TikTok. 
+  voy a empezar a hacer tutoriales de programación en TikTok. ¡Sígueme en <a href="https://www.tiktok.com/@santiagomunarortiz" target="_blank">mi cuenta</a>! (Si te gusta el contenido)
+  ¡Aprenderás cómo construí este portafolio por el que estás navegando actualmente!`,
+  tv: `Esa es mi televisión. Últimamente he estado viendo muchos videos de tecnología en YouTube. Me gusta aprender nuevas técnicas y estar al día con las tendencias.`,
+  bed: `Aquí es donde duermo. Grandes ideas surgen cuando estoy acostado en mi cama. Cuando una idea me viene a la mente, a menudo tengo que escribirla o no podré dormir porque mi energía mental se consume pensando en ella.`,
+  resume: `Este es mi escritorio y sobre él está mi currículum. <a href="https://github.com/SantiagoMunarOrtiz/Resume" target="_blank">¿Quieres echarle un vistazo?</a>
+  ¡Contáctame en mi LinkedIn si tienes alguna oportunidad de trabajo interesante!`,
+  projects: `Información sobre este portafolio: Está hecho con la librería Kaboom.js, que es una librería para hacer juegos en JavaScript.
+  El texto se renderiza con HTML/CSS. Así que el cuadro de texto que estás leyendo actualmente no se renderiza dentro del canvas. Aprende más sobre cómo usar
+  Kaboom.js viendo algunos de mis tutoriales en TikTok <a href="https://www.tiktok.com/@santiagomunarortiz" target="_blank">aquí</a>.`,
+  library: `Hay muchos libros de programación en mis estanterías. Incluso hay uno en ingles (también hablo ingles, por cierto).
+  Probablemente solo he leído uno de ellos. ¿Quién más compra compulsivamente libros técnicos sin terminarlos nunca?`,
+  exit: `Si quieres salir del portafolio de Santiago Munar Ortiz, simplemente cierra la pestaña.`,
+};
